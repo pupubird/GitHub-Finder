430a05f82e68c8d2b189e5211ed39d801842bf60
Rain Chai <rainchai4240@gmail.com>
2019-09-30 00:02:33 +0800

    search result UI

--- ui.js
+++ ui.js
@@ -9,7 +9,7 @@ class UI {
                 <div class="row">
                     <div class="col-md-3">
                         <img class="img-fluid mb-2" src="${user.avatar_url}">
-                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile
+                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile
                         </a>
                     </div>
 
